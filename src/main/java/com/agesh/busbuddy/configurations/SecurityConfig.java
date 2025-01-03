2c6e94a6c8afded439608669c3ae44db4c40c524
Ageshks <ageshksks@gmail.com>
2025-01-03 13:37:36 +0530

    second

--- src/main/java/com/agesh/busbuddy/configurations/SecurityConfig.java
+++ src/main/java/com/agesh/busbuddy/configurations/SecurityConfig.java
@@ -1,5 +1,13 @@
 package com.agesh.busbuddy.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private CustomSucessHandler customSucessHandler;
 
 }
